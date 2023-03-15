54969c317cb504a0757b7d649464f2e84a66fa0c
satoriya <satriya.wicaksana89@gmail.com>
2023-03-15 13:48:13 +0700

    Initial Commit

---
+++ PokemonApiDesign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonApiDesign
+//
+//  Created by Phincon on 15/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
